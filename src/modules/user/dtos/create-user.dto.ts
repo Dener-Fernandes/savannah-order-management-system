6e3f7340b511d2e46693a0d544d36366d0e7d4d7
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2025-02-18 18:07:52 -0400

    feat: add user service and controller

---
+++ src/modules/user/dtos/create-user.dto.ts
@@ -0,0 +1,30 @@
+import { Exclude, Expose } from 'class-transformer';
+import { UserCreatableInterface } from '../interfaces';
+import { IsEmail, IsString } from 'class-validator';
+
+@Exclude()
+export class CreateUserDto implements UserCreatableInterface {
+  @Expose()
+  id?: string;
+
+  @Expose()
+  @IsString()
+  email: string;
+
+  @Expose()
+  @IsString()
+  username: string;
+
+  @Expose()
+  @IsString()
+  password: string;
+
+  @Expose()
+  @IsString()
+  firstName: string;
+
+  @Expose()
+  @IsString()
+  @IsEmail()
+  lastName: string;
+}
---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserService } from './user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    await this.userService.create(createUserDto);
+  }
+
+  @Get()
+  async findAll() {
+    await this.userService.findAll();
+  }
+}
