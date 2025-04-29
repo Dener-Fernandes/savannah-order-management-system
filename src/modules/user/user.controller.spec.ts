26c286f4d3f30351f207bae1734ac44d9abc324d
Dener-Fernandes <dener.fernandes.oliveira@gmail.com>
2025-04-29 00:56:20 -0400

    test: add unit tests for dtos

--- src/modules/user/user.controller.spec.ts
+++ src/modules/user/user.controller.spec.ts
@@ -2,6 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { UserInterface } from './interfaces';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { NotFoundException } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { validate } from 'class-validator';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 let createdUser: Partial<UserInterface> = {
   id: 'ac3a0363-275e-40e6-a3b1-e4783380e89f',
@@ -40,9 +45,9 @@ describe('UserController', () => {
           useValue: {
             create: jest.fn().mockResolvedValue(createdUser),
             findAll: jest.fn().mockResolvedValue(userList),
-            findOne: jest.fn(),
-            update: jest.fn(),
-            remove: jest.fn(),
+            findOne: jest.fn().mockResolvedValue(userList[0]),
+            update: jest.fn().mockResolvedValue(userList[0]),
+            remove: jest.fn().mockResolvedValue(undefined),
           },
         },
       ],
@@ -78,5 +83,112 @@ describe('UserController', () => {
 
     expect(result).toEqual(userList);
     expect(userSerivce.findAll).toHaveBeenCalled();
+  });
+
+  it('should return an user by id successfully', async () => {
+    let id = 'ac3a0363-275e-40e6-a3b1-e4783380e89f';
+
+    const result = await userController.findOne(id);
+
+    expect(result.id).toBe(id);
+    expect(userSerivce.findOne).toHaveBeenCalled();
+  });
+
+  it('should update an user by id successfully', async () => {
+    let id = 'ac3a0363-275e-40e6-a3b1-e4783380e89f';
+
+    let updatedUser: UpdateUserDto = {
+      firstName: 'Richard',
+      active: false,
+    };
+
+    userList[0].firstName = 'Richard';
+    userList[0].active = false;
+
+    const result = await userController.update(id, updatedUser);
+
+    expect(result.id).toBe(id);
+    expect(result.firstName).toBe(updatedUser.firstName);
+    expect(result.active).toBe(updatedUser.active);
+    expect(userSerivce.update).toHaveBeenCalled();
+  });
+
+  it('should delete an user by id successfully', async () => {
+    let id = 'ac3a0363-275e-40e6-a3b1-e4783380e89f';
+
+    const result = await userController.remove(id);
+
+    expect(result).toBeUndefined();
+    expect(userSerivce.remove).toHaveBeenCalled();
+  });
+
+  it('should throw an error if was not able to find and user by id', async () => {
+    const id = 'non-existent-id';
+
+    userSerivce.findOne = jest.fn().mockImplementation(() => {
+      throw new NotFoundException();
+    });
+
+    await expect(userController.findOne(id)).rejects.toThrow(NotFoundException);
+  });
+
+  it('should throw an error if was not able to delete and user by id', async () => {
+    const id = 'non-existent-id';
+
+    userSerivce.remove = jest.fn().mockImplementation(() => {
+      throw new NotFoundException();
+    });
+
+    await expect(userController.remove(id)).rejects.toThrow(NotFoundException);
+  });
+
+  it('should validate successfully with valid data', async () => {
+    const userDto = plainToInstance(CreateUserDto, {
+      email: 'rickDeckard@gmail.com',
+      userName: 'rick-deckard',
+      password: '06102021',
+      firstName: 'Rick',
+      lastName: 'Deckard',
+    });
+
+    const errors = await validate(userDto);
+    expect(errors.length).toBe(0);
+  });
+
+  it('should return error when email is invalid', async () => {
+    const userDto = plainToInstance(CreateUserDto, {
+      email: 'invalid-email',
+      userName: 'rick-deckard',
+      password: '06102021',
+      firstName: 'Rick',
+      lastName: 'Deckard',
+    });
+
+    const errors = await validate(userDto);
+    expect(errors.length).toBeGreaterThan(0);
+    expect(errors.map((e) => e.property)).toContain('email');
+  });
+
+  it('should return error when required fields are not strings', async () => {
+    const userDto = plainToInstance(CreateUserDto, {
+      email: 123,
+      userName: {},
+      password: true,
+      firstName: [],
+      lastName: null,
+    });
+
+    const errors = await validate(userDto);
+    const propertiesWithErrors = errors.map((e) => e.property);
+
+    expect(propertiesWithErrors).toEqual(
+      expect.arrayContaining([
+        'email',
+        'userName',
+        'password',
+        'firstName',
+        'lastName',
+      ]),
+    );
   });
 });
